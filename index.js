206dab44201d20084e5b1c8adedb83d5f55ff0f0
develop <ikhlas.firlana@gmail.com>
2019-02-15 11:01:44 +0700

    ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const yml = require('yaml');
 const fs = require('fs');
 const file = fs.readFileSync('./example/file.yml', 'utf8');
-console.log(yml.parse(file));
+console.log(JSON.stringify(yml.parse(file)));
